a7312c6d5acdf6c53d0864f28e374b7c96957e24
mselrahc <charlesmartino451999@gmail.com>
2023-02-04 15:40:54 +0700

    Add sample auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://191.96.57.123:8000/api/v1/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
